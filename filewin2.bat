70a9370593cecc94cdbae4fe337a49106bfc07eb
roypasaribu <royp_pasaribu@yahoo.com>
2016-04-20 15:43:55 +0700

    Update filewin2.bat

--- filewin2.bat
+++ filewin2.bat
@@ -1,8 +1,8 @@
-<?php
+echo "<?php"
 echo "Masukan Data Anda"
 echo "Nama Anda"
 echo "Npm Anda"
 echo "Kelas Anda"
 echo "Jurusan Anda"
 echo "Jika sudah data yang dimasukan sudah BENAR, Tekan Tombol OK"
-?>
+echo "?>"
